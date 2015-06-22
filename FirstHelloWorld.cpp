4c95113a1ed488bc39959a6cf27e23e5b1f5b32e
13744695 <HelenaRothwell@gmail.com>
2015-06-22 14:19:19 +0100

    First commit

---
+++ FirstHelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//display Hello World to the console
+	cout << "Hello World" <<endl;
+	system("pause");
+	return 0;
+}
---
+++ FirstProgram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>			/* brings in the input output library from C++*/
+using namespace std;
+
+int main ()								/* called the main funtions header Every C++ requires a main function it is the starting point*/
+{										/* the main funtion's body*/
+	cout << "C++ is fun! \n";			/* cout stands for character output Stream allows us to display characters to the screen*/
+	return 0;							/* << these are called the insertion operator this takes everything on the right and redirects it to what we have on the left hand side \n is a newline  */
+}										/* all c++ need to be terminated by a semi colon */
+										/* returns the value of zero. the int above says it returns something of type int. REturning zero means it ran correctly */
+										
